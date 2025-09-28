9b41827891504d346d635ae4455adae38a49465b
ordi <121055982+ordiha@users.noreply.github.com>
2025-09-28 11:02:57 +0200

    Update GiveawayNFT.sol
    modified to make verification simply

--- contracts/GiveawayNFT.sol
+++ contracts/GiveawayNFT.sol
@@ -1,19 +1,90 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract GiveawayNFT {
+    string private _name;
+    string private _symbol;
+    string private _baseURI;
 
-contract GiveawayNFT is ERC721URIStorage, Ownable {
-    uint256 public nextTokenId;
+    uint256 private _tokenIds;
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
 
-    constructor() ERC721("GiveawayNFT", "GNFT") Ownable(msg.sender) {}
+    address public admin;
 
-    function giveawayMint(address[] calldata recipients, string memory baseURI) external onlyOwner {
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event BaseURIChanged(string newBaseURI);
+
+    constructor(string memory name_, string memory symbol_, string memory baseURI_) {
+        _name = name_;
+        _symbol = symbol_;
+        _baseURI = baseURI_;
+        admin = msg.sender;
+    }
+
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    function baseURI() external view returns (string memory) {
+        return _baseURI;
+    }
+
+    function setBaseURI(string memory newBase) external {
+        require(msg.sender == admin, "Not admin");
+        _baseURI = newBase;
+        emit BaseURIChanged(newBase);
+    }
+
+    function balanceOf(address ownerAddr) external view returns (uint256) {
+        require(ownerAddr != address(0), "Zero address");
+        return _balances[ownerAddr];
+    }
+
+    function ownerOf(uint256 tokenId) external view returns (address) {
+        address ownerAddr = _owners[tokenId];
+        require(ownerAddr != address(0), "Invalid tokenId");
+        return ownerAddr;
+    }
+
+    function giveawayMint(address[] memory recipients) external {
+        require(msg.sender == admin, "Only admin can mint");
         for (uint256 i = 0; i < recipients.length; i++) {
-            _mint(recipients[i], nextTokenId);
-            _setTokenURI(nextTokenId, string(abi.encodePacked(baseURI, "/", Strings.toString(nextTokenId))));
-            nextTokenId++;
+            _tokenIds++;
+            uint256 newId = _tokenIds;
+            _owners[newId] = recipients[i];
+            _balances[recipients[i]] += 1;
+            emit Transfer(address(0), recipients[i], newId);
         }
+    }
+
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(_owners[tokenId] != address(0), "Invalid tokenId");
+        return string(abi.encodePacked(_baseURI, uint2str(tokenId), ".json"));
+    }
+
+    // utils
+    function uint2str(uint256 _i) internal pure returns (string memory) {
+        if (_i == 0) return "0";
+        uint256 j = _i;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        j = _i;
+        while (j != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(j - j / 10 * 10));
+            bstr[k] = bytes1(temp);
+            j /= 10;
+        }
+        return string(bstr);
     }
 }
