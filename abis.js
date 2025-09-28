c1496c21d2f80037693371efe74c9ae69d7099f9
ordi <121055982+ordiha@users.noreply.github.com>
2025-09-28 20:02:29 +0200

    Create abis.js
    abis.js created

---
+++ abis.js
@@ -0,0 +1,42 @@
+const abis = {
+  GiveawayNFT: {
+    address: "0xF3b6f93110e308Acc02f70E36F9F031731f7f5E7",
+    abi: GiveawayNFT_ABI
+  },
+  DonationTracker: {
+    address: "0x50D7116D6Ec5Dca879577A24D7B0FEeF3f3E2f79",
+    abi: DonationTracker_ABI
+  },
+  VotingSystem: {
+    address: "0xB1e1c86e261c9D296ef3d47E4EDDefa644D1e5e1",
+    abi: VotingSystem_ABI
+  },
+  Escrow: {
+    address: "0xF8ca793D9f51b5F2aFb7a4E3922D2DA34B570C58",
+    abi: Escrow_ABI
+  },
+  SubscriptionService: {
+    address: "0x099f156896e7f96F7aFc78Aac931df6f41102fB3",
+    abi: SubscriptionService_ABI
+  },
+  Crowdfunding: {
+    address: "0x0Fc92a079Ff4Da066d62b4e86ddE23F48a3FF6bf",
+    abi: Crowdfunding_ABI
+  },
+  Lottery: {
+    address: "0x9a22a1629623C9DE7FDEb13CF1D9c95B46d6CfA4",
+    abi: Lottery_ABI
+  },
+  MessageBoard: {
+    address: "0x407383E81112a2818468Bf0A01F354223E0B7A1C",
+    abi: MessageBoard_ABI
+  },
+  OpinionRegistry: {
+    address: "0x2A834aa557261E4143D0dfeFbc30B5FC7a1A0bE3",
+    abi: OpinionRegistry_ABI
+  },
+  GuessNumber: {
+    address: "0x6fC196e5D2b9e30D89673611Ab5594957e5529C2",
+    abi: GuessNumber_ABI
+  }
+};
